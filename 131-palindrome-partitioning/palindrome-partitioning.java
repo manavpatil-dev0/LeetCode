c3cf33546827b50861818b938ce0957ac630d093
Manav Patil <manavpatil.dev@gmail.com>
2026-08-12 22:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 71.4 MB (5.08%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,57 +1,56 @@
-import java.util.*;
-
+import java.util.AbstractList;
 class Solution {
+    private List<List<String>> myList;
     public List<List<String>> partition(String s) {
-
-        List<List<String>> result = new ArrayList<>();
-
-        backtrack(s, 0, new ArrayList<>(), result);
-
-        return result;
-    }
-
-    private void backtrack(
-            String s,
-            int start,
-            List<String> current,
-            List<List<String>> result) {
-
-        // Entire string has been partitioned
-        if (start == s.length()) {
-            result.add(new ArrayList<>(current));
-            return;
-        }
-
-        for (int end = start; end < s.length(); end++) {
-
-            if (isPalindrome(s, start, end)) {
-
-                current.add(s.substring(start, end + 1));
-
-                backtrack(
-                    s,
-                    end + 1,
-                    current,
-                    result
-                );
-
-                current.remove(current.size() - 1);
+         return new AbstractList<List<String>>(){
+            @Override 
+            public int size(){
+                init();
+                return myList.size();
             }
-        }
-    }
-
-    private boolean isPalindrome(String s, int left, int right) {
-
-        while (left < right) {
-
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
+            @Override
+            public List<String> get (int i){
+                init();
+                return myList.get(i);
             }
 
-            left++;
-            right--;
-        }
+            private void init(){
+                if (myList != null) return;
+                myList = new ArrayList<>();
+                generator(new ArrayList<String>(), s, "", 0);
+            }
+        };
+    }
 
+    private void generator(List<String> list, String s, String a, int i){
+        if (i == s.length()){
+            if (!a.isEmpty() && checker(a)) {
+                list.add(a);
+                myList.add(new ArrayList<>(list));
+                list.remove(list.size()-1);
+                return;
+            }
+            else {
+                return;
+            }
+        } 
+        a+=s.charAt(i);
+        if (checker(a)){
+            list.add(a);
+            generator(list, s, "", i+1);
+            list.remove(list.size()-1);
+        }
+        generator(list, s, a, i+1);
+    }
+
+    private boolean checker(String a){
+        int i = 0;
+        int j = a.length()-1;
+        while (i < j){
+            if (a.charAt(i) != a.charAt(j)) return false;
+            i++;
+            j--;
+        }
         return true;
     }
 }
