3c4d63279adb476aebcb1a02ce0ba2ab406ef24c
Manav Patil <manavpatil.dev@gmail.com>
2026-08-31 19:04:57 +0530

    Time: 3 ms (92.77%) | Memory: 45.3 MB (36.40%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        // Find next greater element for every value in nums2
+        for (int num : nums2) {
+
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+
+            stack.push(num);
+        }
+
+        // Elements remaining in stack have no greater element
+        while (!stack.isEmpty()) {
+            nextGreater.put(stack.pop(), -1);
+        }
+
+        // Build answer for nums1
+        int[] answer = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            answer[i] = nextGreater.get(nums1[i]);
+        }
+
+        return answer;
+    }
+}
